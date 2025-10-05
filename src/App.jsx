7d45b79f3776728442f0483561e0807803a872ae
maruthishalivahana <maruthikummari553@gmail.com>
2025-10-05 13:13:29 +0530

    settingUp application Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import DoctorAddingForm from './Components/DoctorAddingForm'
+import { Provider } from "react-redux";
+import { store } from "./utils/appStore";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from './Components/Home';
+import { AboutUs } from './Components/AboutUs';
+import PatientInfo from './Components/PatientInfo';
+import Specialities from './Components/Specialities';
+import KnowledgeCenter from './Components/KnowledgeCenter';
+import OurDoctors from './Components/OurDoctors';
 
 function App() {
-
   return (
-    <DoctorAddingForm />
-  )
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          {/* Home is the layout wrapper with Navbar + Footer */}
+          <Route path="/" element={<Home />}>
+            <Route index element={<AboutUs />} />            {/* default page */}
+            <Route path="aboutus" element={<AboutUs />} />
+            <Route path="ourdoctors" element={<OurDoctors />} />
+            <Route path="patientinfo" element={<PatientInfo />} />
+            <Route path="specialities" element={<Specialities />} />
+            <Route path="knowledgecenter" element={<KnowledgeCenter />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
